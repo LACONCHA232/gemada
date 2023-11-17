b24f2381ed228f3e969028eadede3cb4e2f194cf
Mario Alberto Jigamara <jigm970907@gmail.com>
2023-11-16 18:05:45 -0600

    Interfaz lista al 90 % en funcionalidad

--- script/interfaz-main.gd
+++ script/interfaz-main.gd
@@ -8,7 +8,20 @@ func _ready():
 #Escucha el evento del boton "inicar partidas" para que cuando sea precionado ejecute el juego
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://escenas/nivel_1.tscn")
-	
-	
- 
- 
+
+#Escucha el evento del boton "personaje" para que cuando sea precionado ejecute la ventana personaje
+func _on_pressed():
+	get_tree().change_scene_to_file("res://escenas/interfaz-personaje.tscn")
+
+#Escucha el evento del boton "ajustes" para que cuando sea precionado ejecute la ventana ajustes
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://escenas/interfaz-ajustes.tscn")
+
+#Esta funcion le da la funcionalidad al boton de salir
+func _on_close_button_pressed():
+	get_tree().change_scene_to_file("res://escenas/interfaz-main.tscn")
+
+
+#el button se refiere al boton de jugar
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://escenas/nivel_1.tscn")
