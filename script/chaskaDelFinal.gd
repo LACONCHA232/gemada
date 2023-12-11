217718e53f33d706920579a6d82b174855861fe5
Diego C. <diegodiazdelaconcha93@gmail.com>
2023-12-10 18:15:16 -0600

    Se agrego chaska Mayor para la finalizacion del nivel 2

---
+++ script/chaskaDelFinal.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+func _on_body_entered(body):
+	if body.get_name() == "Player2":
+		#emit_signal("chaskaCollected")
+		queue_free()
+		get_tree().change_scene_to_file("res://escenas/interfaz-level.tscn")
