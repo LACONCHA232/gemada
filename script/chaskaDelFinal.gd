7d055eae788c2a5336e15eeb389304a2bda5c002
Mario Alberto Jigamara <jigm970907@gmail.com>
2023-12-11 00:08:27 -0600

    se arreglo el cagadero que se hizo con el merge

--- script/chaskaDelFinal.gd
+++ script/chaskaDelFinal.gd
@@ -5,4 +5,4 @@ func _on_body_entered(body):
 	if body.get_name() == "Player2":
 		#emit_signal("chaskaCollected")
 		queue_free()
-		get_tree().change_scene_to_file("res://escenas/interfaz-level.tscn")
+		get_tree().change_scene_to_file("res://escenas/creditos.tscn")
